bdf3251cf7def0cbd48a50c58e33155dd8cb0e68
salah <salah.kadd4@gmail.com>
2026-04-18 20:21:29 +0100

    prblm mainActivity java

--- boutique-en-ligne/android/app/src/main/java/com/cabastore/app/MainActivity.java
+++ boutique-en-ligne/android/app/src/main/java/com/cabastore/app/MainActivity.java
@@ -1,16 +1,25 @@
 package com.cabastore.app;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.webkit.CookieManager;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    registerPlugin(SocialLoginPlugin.class);
+
     SharedPreferences prefs = getSharedPreferences("app_prefs", MODE_PRIVATE);
     boolean isFirstLaunch = prefs.getBoolean("first_launch", true);
-
     if (isFirstLaunch) {
       CookieManager cookieManager = CookieManager.getInstance();
       cookieManager.removeAllCookies(null);
@@ -19,5 +28,21 @@ public class MainActivity extends BridgeActivity {
     }
 
     super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (
+      requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN &&
+      requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX
+    ) {
+      PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+      if (pluginHandle == null) return;
+      SocialLoginPlugin plugin = (SocialLoginPlugin) pluginHandle.getInstance();
+      if (plugin == null) return;
+      plugin.handleGoogleLoginIntent(requestCode, resultCode, data);
+    }
   }
 }
